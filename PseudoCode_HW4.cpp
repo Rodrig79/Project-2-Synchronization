3523b41882a583e36b1fb4e6a534516967ab5213
Marco Rodriguez <48032991+Rodrig79@users.noreply.github.com>
2020-02-01 16:26:37 -0800

    Add files via upload

---
+++ PseudoCode_HW4.cpp
@@ -0,0 +1,48 @@
+/*
+Consumer Wait:
+1) Car in interesction traverling opposite direction that wants to take from
+2) No cars
+
+Producer Wait:
+Never, unbounded
+*/
+#include <iostream>
+#include <phtread.h>
+#include <semaphore.h>
+
+/*
+Shared Variables:
+BuffN;
+BuffS
+BuffMutex
+BuffMutex
+BuffNSize
+BuffSSize
+*/
+
+
+//Consumer:
+wait(empty);
+wait(mutex);
+//access buffer
+if(size == 0){
+    signal()
+
+}
+wait(mutex)
+
+
+wait(empty)
+wait(Buf[side])
+wait(Buf[side])
+if(Buf[side].size()==0||Buf[!side].size>=10){
+    [size!=side];
+}
+signal(Buf[!side]);
+car = Buf[side].pop();
+if(Buf[side]).size!=0)
+    signal(empty)
+signal(Buf[side]);
+
+//Consume Car here
+}

b81ca44e23287b5fdf3c552c9ea41eebb72c2a17
ndoub <60121943+ndoub@users.noreply.github.com>
2020-02-03 15:50:51 -0800

    Template for the main methods of our program
    All of them have comments above saying what they do and what exactly the parameter list looks like

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <queue>
+
+int main(){
+	std::queue<int> test;
+	return 0;
+}
+
+//parameter is an array of the format [buffer, lock for buffer]
+//buffer is a queue
+
+
+//spawning cars
+//signaler
+//intersection
+
+/*
+	spawns cars according to the rules specified and inserts them at the back of a queue
+	param = [buf, bufLock, emptyLock]
+*/
+
+/*
+	signals cars taking from both sides according to specified rules
+	param = [buf[2], bufLock[2], emptyLock, intersectCount,intersectCountLock, intersectLock]
+*/
+void* carSpawn(void* param) {
+	
+}
+
+void* signaler(void* param) {
+	
+}
+
+/*
+	Sleeps for 1000 ms then writes the car to output file
+	param = [car, intersectCount, intersectCountLock, intersectLock, fileLock]
+*/
+void* intersection(void* param) {
+	
+}
+
+struct Car {
+	int val = 0;
+};

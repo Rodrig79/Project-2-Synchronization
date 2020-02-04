bbd827ef67c85acc43200e0c096e067e7e1af7b0
Marco Rodriguez <48032991+Rodrig79@users.noreply.github.com>
2020-02-03 17:33:14 -0800

    Added pthread_sleep

--- main.cpp
+++ main.cpp
@@ -34,6 +34,26 @@ void* carSpawn(void* param) {
 	param = [buf[2], bufLock[2], emptyLock, intersectCount,intersectCountLock, intersectLock, fileLock]
 */
 
+int pthread_sleep(int seconds)
+{
+    pthread_mutex_t mutex;
+    pthread_cond_t conditionvar;
+    struct timespec timetoexpire;
+    if (pthread_mutex_init(&mutex, NULL))
+    {
+        return -1;
+    }
+    if (pthread_cond_init(&conditionvar, NULL))
+    {
+        return -1;
+    }
+    //When to expire is an absolute time, so get the current time and add
+    //it to our delay time
+    timetoexpire.tv_sec = (unsigned int)time(NULL) + seconds;
+    timetoexpire.tv_nsec = 0;
+    return pthread_cond_timedwait(&conditionvar, &mutex, &timetoexpire);
+}
+
 void* signaler(void* param) {
 	//0 is North 1 is South
 	
